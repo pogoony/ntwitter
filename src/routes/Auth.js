e8f197459f4706839bf2fb29125fb17d3f9cda6d
이현철 <lhc9321@icloud.com>
2020-10-20 14:43:17 +0900

    master - login form part one

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [Email, setEmail] = useState();
+    const [Password, setPassword] = useState();
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('click submit');
+    };
+    const onChange = (e) => {
+
+        const {
+            target: { name, value }
+        } = e;
+
+        if (name === 'email') {
+            console.log(value);
+            setEmail();
+        } else if (name === 'password') {
+            console.log(value);
+            setPassword();
+        }
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="email"
+                    value={Email}
+                    onChange={onChange}
+                    required
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={Password}
+                    onChange={onChange}
+                    required
+                />
+
+                <input type="submit" value="Log In"></input>
+                <div>
+                    <button>Continue with Google</button>
+                    <button>Continue with Github</button>
+                </div>
+            </form>
+        </div>
+    );
+
+};
+
 export default Auth;
